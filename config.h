450afd4cdeb9d0fc7c7db22d6d9c7ccf0de434ae
hal3e <git@hal3e.io>
2023-10-30 18:56:14 +0100

    update config

--- config.h
+++ config.h
@@ -1,21 +1,21 @@
 /* user and group to drop privileges to */
 static const char *user  = "nobody";
-static const char *group = "nobody";
+static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "#000000",   /* after initialization */
 	[INPUT] =  "#282c34",   /* during input */
-	[FAILED] = "#be5046",   /* wrong password */
+	[FAILED] = "#000000",   /* after initialization */
 };
 
 /* lock screen opacity */
 static const float alpha = 0.5;
 
 /* treat a cleared input like a wrong password (color) */
-static const int failonclear = 1;
+static const int failonclear = 0;
 
 /* default message */
-static const char * message = "Enter password to unlock";
+static const char * message = "";
 
 /* text color */
 static const char * text_color = "#abb2bf";
